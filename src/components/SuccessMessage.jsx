1b12f1f4a6a98c1247f28652f9229f8e137fd494
Roksolana Veres <Roxolanka1@ukr.net>
2024-06-14 15:27:20 +0300

    success message disappearance with useEffect and css classes

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react";
+
 export default function SuccessMessage({ enteredData }) {
+  const [showSuccess, setShowSuccess] = useState(true);
   const { firstName, email } = enteredData;
 
+  useEffect(() => {
+    const timeoutID = setTimeout(() => setShowSuccess(false), 3500);
+    return () => clearTimeout(timeoutID);
+  }, []);
+
   return (
-    <div className="success-message">
+    <div
+      className={`success-message ${
+        showSuccess ? "success-message--visible" : "success-message--hidden"
+      }`}
+    >
       <h2 className="success-header">Message Sent!</h2>
       <p className="success-text">
-        Thank you, {firstName}, for completing the form. We'll send you a confirmation
-        letter to your email: {email}.
+        Thank you, {firstName}, for completing the form. We'll send you a confirmation letter to
+        your email: {email}.
       </p>
     </div>
   );
